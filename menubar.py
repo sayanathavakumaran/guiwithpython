9e4f37735b25035d320e36ed34948277ca4dd04c
sayanathavakumaran <sagekumar@gmail.com>
2025-11-08 17:59:10 +0000

    Add files via upload

---
+++ menubar.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter.ttk import *
+
+screen = Tk()
+screen.geometry("350x250")
+
+screen.title("menu")
+menubar = Menu(screen)
+
+#adding menu options
+file = Menu(menubar,tearoff=0)
+menubar.add_cascade(label="file",menu=file)
+file.add_command(label="new",command=None)
+file.add_command(label="open",command=None)
+file.add_command(label="save",command=None)
+file.add_command(label="share",command=None)
+
+edit = Menu(menubar,tearoff=0)
+menubar.add_cascade(label="edit",menu=edit)
+edit.add_command(label="undo",command=None)
+edit.add_command(label="redo",command=None)
+edit.add_command(label="cut",command=None)
+edit.add_command(label="copy",command=None)
+edit.add_command(label="paste",command=None)
+
+screen.config(menu=menubar)
+screen.mainloop()
+
+
+
