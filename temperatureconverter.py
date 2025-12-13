4f17cbf421e995924f432f99e2ba81302c57997a
sayanathavakumaran <sagekumar@gmail.com>
2025-12-13 17:10:44 +0000

    temperatureconverter.py

--- temperatureconverter.py
+++ temperatureconverter.py
@@ -4,6 +4,14 @@ screen = Tk()
 screen.geometry("500x400")
 screen.config(background="mistyrose")
 
+def convert():
+    entryboxstore = entrybox.get()
+    entryboxstore = float(entryboxstore)
+    conversion = (entryboxstore*(9/5))+32
+    conversion = round(conversion,1)
+    resulttext.delete("1.0",END)
+    resulttext.insert(END,conversion)
+
 titlelabel = Label(screen,text="CELSIUS TO FAHRENHEIT",background="mistyrose",font=("calibri",17))
 titlelabel.place(x=140,y=30)
 questionlabel = Label(screen,text="enter the temperature in celsius: ",background="mistyrose",font=("calibri",11))
@@ -12,7 +20,9 @@ entrybox = Entry(screen,text="",background="white",font=("calibri",11))
 entrybox.place(x=300,y=130)
 resultlabel = Label(screen,text="temperature in fahrenheit: ",background="mistyrose",font=("calibri",11))
 resultlabel.place(x=80,y=200)
-convertbutton = Button(screen,text="   convert   ",background="white")
+resulttext = Text(screen,height=1.35,width=17)
+resulttext.place(x=300,y=200)
+convertbutton = Button(screen,text="   convert   ",background="white",command=convert)
 convertbutton.place(x=210,y=285)
 
 
